4f8e5be44cb2abcc35417890ee9b0c074054aa59
MORILLE Jérôme <morille.jerome@groupagrica.com>
2020-03-05 11:33:43 +0100

    merge release

--- ossrh-release.sh
+++ ossrh-release.sh
@@ -10,3 +10,7 @@ git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
 git checkout master
 git branch -D prepare-release
+
+# Not work pour 0-M3
+# git merge ossrh-$VERSION
+# mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DgenerateBackupPoms=false -DnextSnapshot=true
