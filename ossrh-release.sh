ec777d5163d012b098d890b44f68f3b42053641f
MORILLE Jerome <morille.jerome@groupagrica.com>
2022-09-05 12:31:17 +0200

    script for release in other branch than master

--- ossrh-release.sh
+++ ossrh-release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+BRANCH_NAME="release/2.4.x"
 VERSION=${1}
  if [[ -z "${VERSION}" ]]; then
     read -p "Version à releaser ? " VERSION
@@ -11,11 +12,11 @@ mvn clean install
 git commit -am "Prepare Release version $VERSION"
 git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
-git checkout master
+git checkout ${BRANCH_NAME}
 git branch -D prepare-release
 
 # Not work pour 0-M3
 git merge ossrh-$VERSION
-mvn org.codehaus.mojo:versions-maven-plugin:2.7:set -DgenerateBackupPoms=false -DnextSnapshot=true
+mvn org.codehaus.mojo:versions-maven-plugin:2.11.0:set -DgenerateBackupPoms=false -DnextSnapshot=true
 git commit -am "Prepare Next Snapshot"
-git push origin master
+git push origin ${BRANCH_NAME}
