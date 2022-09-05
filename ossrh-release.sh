3856f1dc5f8a22f2ad155532e38d1ee43e4ee329
MORILLE Jerome <morille.jerome@groupagrica.com>
2022-09-05 13:55:09 +0200

    update release script

--- ossrh-release.sh
+++ ossrh-release.sh
@@ -6,17 +6,26 @@ VERSION=${1}
     read -p "Version à releaser ? " VERSION
  fi
 
+echo "1- Create branch 'prepare-release'"
 git co -b prepare-release
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$VERSION
+
+echo "2- Set Maven Version $VERSION"
+mvn org.codehaus.mojo:versions-maven-plugin:2.11.0:set -DgenerateBackupPoms=false -DnewVersion=$VERSION
 mvn clean install
 git commit -am "Prepare Release version $VERSION"
+
+echo "3- Create Maven Tag $VERSION"
 git tag -a ossrh-$VERSION -m "Release version $VERSION"
 git push origin ossrh-$VERSION
+
+echo "4- Delete branch 'prepare-release'"
 git checkout ${BRANCH_NAME}
 git branch -D prepare-release
 
 # Not work pour 0-M3
 git merge ossrh-$VERSION
+
+echo "6- Increment Version '${BRANCH_NAME}'"
 mvn org.codehaus.mojo:versions-maven-plugin:2.11.0:set -DgenerateBackupPoms=false -DnextSnapshot=true
 git commit -am "Prepare Next Snapshot"
 git push origin ${BRANCH_NAME}
