52b911faa1f401ca1077c85b986af578e11923b3
MORILLE Jerome <morille.jerome@groupagrica.com>
2022-12-22 17:53:09 +0100

    Add build ci

---
+++ Jenkinsfile-ci.groovy
@@ -0,0 +1,36 @@
+pipeline {
+    agent { label 'built-in' }
+
+    options {
+        timestamps()
+        ansiColor('xterm')
+        disableConcurrentBuilds()
+        timeout(time: 30, unit: 'MINUTES')
+        buildDiscarder(logRotator(numToKeepStr: '5'))
+    }
+
+    stages {
+        stage('Cleanup') {
+            steps {
+                deleteDir()
+            }
+        }
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Clean deploy') {
+            steps {
+                script {
+                    withMaven(globalMavenSettingsConfig: 'maven-global-settings', mavenSettingsConfig: 'socle-maven-settings', jdk: 'OpenJDK 11.x', maven: 'Maven 3.x') { 
+                        sh "mvn clean deploy"
+                    }
+                }
+            }
+        }
+
+    }
+}
